5bab57953e602041112b454c8b4253926d081322
unknown <patrykbarnas@yahoo.com>
2024-09-25 08:22:18 -0500

    application layout/routing complete.

---
+++ src/app/lib/defintions.ts
@@ -0,0 +1,11 @@
+export type Calories = {
+  id: number;
+  name: string;
+  calories: number;
+  protein: number;
+  fat: number;
+  carbs: number;
+  servingSize: string;
+  mealType: string;
+  dateLogged: Date;
+};
