02ddb47ad883a138a15668000c8a50064cf4e90e
unknown <patrykbarnas@yahoo.com>
2024-09-27 10:06:27 -0500

    added form state to remember previous selection

--- src/app/survey/page.tsx
+++ src/app/survey/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 
 export default function Page() {
   const [step, setStep] = useState<number>(0);
+  const [formData, setFormData] = useState({
+    age: "",
+    weight: "",
+    gender: "",
+    height: "",
+  });
 
   const handleNextStep = (): void => {
     setStep((current) => current + 1);
@@ -14,39 +20,81 @@ export default function Page() {
     }
   };
 
+  const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { id, value, name, type } = e.target;
+
+    setFormData((prev) => ({
+      ...prev,
+      [type === "radio" ? name : id]: value,
+    }));
+  };
+
   return (
     <form>
       <div>
         {step === 0 && (
           <div>
             <label htmlFor="age">How old are you?</label>
-            <input id="" />
+            <input id="age" onChange={handleFormChange} value={formData.age} />
           </div>
         )}
         {step === 1 && (
           <div>
             <label htmlFor="weight">How much do you currently weight?</label>
-            <input id="weight" />
+            <input
+              id="weight"
+              onChange={handleFormChange}
+              value={formData.weight}
+            />
           </div>
         )}
         {step === 2 && (
           <div>
             <label htmlFor="gender">What is your gender?</label>
-            <input id="gender" />
+            <div>
+              <label htmlFor="female">Female</label>
+              <input
+                id="female"
+                type="radio"
+                name="gender"
+                value="female"
+                checked={formData.gender === "female"}
+                onChange={handleFormChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="male">Male</label>
+              <input
+                id="male"
+                type="radio"
+                name="gender"
+                value="male"
+                checked={formData.gender === "male"}
+                onChange={handleFormChange}
+              />
+            </div>
           </div>
         )}
         {step === 3 && (
           <div>
             <label htmlFor="height">What is your height?</label>
-            <input id="height"></input>
+            <input
+              id="height"
+              onChange={handleFormChange}
+              value={formData.height}
+            />
           </div>
         )}
-        <button type="button" onClick={handlePrevStep}>
-          {"<-"}
-        </button>
-        <button type="button" onClick={handleNextStep}>
-          {"->"}
-        </button>
+        {step === 0 ? null : (
+          <button type="button" onClick={handlePrevStep}>
+            {"<-"}
+          </button>
+        )}
+        {step === 3 ? null : (
+          <button type="button" onClick={handleNextStep}>
+            {"->"}
+          </button>
+        )}
       </div>
     </form>
   );
